bd2b6e23ae9954d8f67337fb43c7220513f8832c
Marko Mitic <marco89nish@outlook.com>
2024-12-05 00:38:51 -0800

    Update Day3 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,29 +1,23 @@
 fun main() {
-    val mulRegex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
-    val multiRegex = Regex("(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\))")
+    val mulRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+    val multiRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
 
     fun part1(memory: String): Int =
         mulRegex.findAll(memory)
-            .map { matchResult -> matchResult.value }
-            .map { exp -> exp.substring(4, exp.length - 1) }
-            .map { csv -> csv.split(',') }
-            .sumOf { vals -> vals[0].toInt() * vals[1].toInt() }
+            .sumOf { match -> match.groupValues[1].toInt() * match.groupValues[2].toInt() }
 
     fun part2(memory: String): Int {
         var enabled = true
         return multiRegex.findAll(memory)
-            .map { matchResult -> matchResult.value }
             .filter {
-                when (it) {
+                when (it.value) {
                     "do()" -> false.also { enabled = true }
                     "don't()" -> false.also { enabled = false }
                     else -> true
                 }
             }
             .filter { enabled }
-            .map { exp -> exp.substring(4, exp.length - 1) }
-            .map { csv -> csv.split(',') }
-            .sumOf { vals -> vals[0].toInt() * vals[1].toInt() }
+            .sumOf { match -> match.groupValues[1].toInt() * match.groupValues[2].toInt() }
     }
 
     // Read the input from the `src/Day##.txt` file.
